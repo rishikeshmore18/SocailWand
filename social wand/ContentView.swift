323d4fd33f43360c31a8b4dc306680c6109c7aad
rao <raotrishali0106@gmail.com>
2025-11-06 21:34:41 -0500

    Initial Commit

---
+++ social wand/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  social wand
+//
+//  Created by Trishali Rao on 11/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ social wand/social_wandApp.swift
@@ -0,0 +1,17 @@
+//
+//  social_wandApp.swift
+//  social wand
+//
+//  Created by Trishali Rao on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct social_wandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
